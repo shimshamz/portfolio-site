8eed5e5a9d13b1ecb0f103b65662f771243bef54
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-12-10 14:23:55 -0500

    Setup Firebase

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAoJXeBndGCWlUq6pzA3dQY-KYaydAp3fA",
+    authDomain: "portfolio-shimshamz.firebaseapp.com",
+    databaseURL: "https://portfolio-shimshamz.firebaseio.com",
+    projectId: "portfolio-shimshamz",
+    storageBucket: "portfolio-shimshamz.appspot.com",
+    messagingSenderId: "787435577824",
+    appId: "1:787435577824:web:1ce17e6d18a1de4f91cb85"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
