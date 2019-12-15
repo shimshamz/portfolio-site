b5035677048723d59cd27fb9a6d972c1e8d5e429
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-12-14 20:18:31 -0500

    Added firebase database and storage

--- js/upload.js
+++ js/upload.js
@@ -3,6 +3,8 @@ const porfolioFields = document.getElementById('portfolioFields');
 const photoFields = document.getElementById('photoFields');
 const imageUploadBtn = document.getElementById('imageUpload');
 const imageName = document.getElementById('imageName');
+let file;
+const submitBtn = document.getElementById('submitBtn');
 
 uploadType.addEventListener('change', e => {
     const value = e.target.value;
@@ -15,21 +17,62 @@ uploadType.addEventListener('change', e => {
     }
 });
 
+
+
+
 imageUploadBtn.addEventListener('change', e => {
     const files = e.target.files;
-    console.log(files[0]);
-    imageName.textContent = files[0].name;
-    const formData = new FormData();
-    formData.append('image', files[0]);
-
-    fetch('upload.php', {
-        method: 'POST',
-        body: formData
-    })
-    .then(res => res.text())
-    .then(data => {
-        return data;
-    })
-    .catch(console.error);
+    file = files[0];
+    console.log(file);
+    imageName.textContent = file.name;
 });
 
+submitBtn.addEventListener('click', handleFormSubmit);
+
+
+
+function getAllFields() {
+    var form = new Object();
+    const title = getInputVal('title');
+    form['title'] = title;
+    form['imgLink'] = (file ? 'portfolio-items/' + file.name : "");
+    if (uploadType.value == 'portfolio') {
+        const link = getInputVal('link');
+        const githubLink = getInputVal('githubLink');
+        const technologies = getInputVal('technologies');
+        const itemType = getInputVal('item-type');
+        const description = getInputVal('description');
+        Object.assign(form, {
+            'link': link, 
+            'githubLink': githubLink,
+            'technologies': technologies,
+            'itemType': itemType,
+            'description': description
+        });
+    } else if (uploadType.value == 'photo') {
+        const location = getInputVal('location');
+        Object.assign(form, {
+            'location': location
+        });
+    }
+    console.log(form);
+    return form;
+}
+
+function handleImageUpload() {
+    let storageRef = storage.ref('portfolio-items/' + file.name);
+    storageRef.put(file);
+}
+
+function handleFormSubmit() {
+    const form = getAllFields();
+    db.collection('portfolio-items').add(form);
+    if (file) {
+        handleImageUpload();
+    }
+    
+}
+
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
